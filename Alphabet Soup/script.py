8c321ff0edbb28b1f4910dbd4f871263cd5aabd7
David <gerhkindidnothingwrong@gmail.com>
2024-05-09 22:12:53 +0100

    Include return statement to make default script run

--- Alphabet Soup/script.py
+++ Alphabet Soup/script.py
@@ -5,4 +5,5 @@ key = "PIRfjL7cksiO2pZhH_zY}AUSxl0DCQTeMwVBbyn9JKq1oavrt53F8Gd{ENWuX46gm"
 output = "w0CboJCgCaem{X{4eNXCN{8C{8d{TCQTWDeWEXTTuTdu6"
 
 def decrypt(cipher, key):
-    # I can't remember what this cipher was called but something to do with the alphabet being equal to a different letter
+	#I can't remember what this cipher was called but something to do with the alphabet being equal to a different letter
+    return
