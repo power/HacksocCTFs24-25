282837f707cdd7555b2ac7b9ffc8d6808f797e6d
David <gerhkindidnothingwrong@gmail.com>
2024-05-09 22:06:02 +0100

    Add files via upload

---
+++ Alphabet Soup/script.py
@@ -0,0 +1,8 @@
+import random
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890}_{"
+key = "PIRfjL7cksiO2pZhH_zY}AUSxl0DCQTeMwVBbyn9JKq1oavrt53F8Gd{ENWuX46gm"
+output = "w0CboJCgCaem{X{4eNXCN{8C{8d{TCQTWDeWEXTTuTdu6"
+
+def decrypt(cipher, key):
+    # I can't remember what this cipher was called but something to do with the alphabet being equal to a different letter
